90ee37b2b71ff80333392c652f65e12767808d52
pwoodr01 <robert.wood@usda.gov>
2026-01-27 15:21:51 -0600

    Car class

--- Chapter5/SimpleClassExample/Car.cs
+++ Chapter5/SimpleClassExample/Car.cs
@@ -6,6 +6,23 @@ class Car
     public string petName;
     public int currSpeed;
 
+    // Default constructor.
+    public Car() {}
+
+    // Constructor with only petName, uses
+    // default int value for currSpeed
+    public Car(string pn)
+    {
+        petName = pn;
+    }
+
+    // Let caller set both fields.
+    public Car(string pn, int cs)
+    {
+        petName = pn;
+        currSpeed = cs;
+    }
+
     // The 'behavior' of the Car.
     public void PrintState()
         => Console.WriteLine($"{petName} is going {currSpeed} MPH.");
--- Chapter5/SimpleClassExample/Program.cs
+++ Chapter5/SimpleClassExample/Program.cs
@@ -4,8 +4,6 @@ Console.WriteLine("***** Fun with Class Types *****\n");
 
 // Allocate and configure a Car object.
 Car myCar = new Car();
-myCar.petName = "Henry";
-myCar.currSpeed = 10;
 
 // Speed up the car a few times and print out the new state.
 for (int i = 0; i < 10; i++)
