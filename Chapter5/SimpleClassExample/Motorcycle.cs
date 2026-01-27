1263810504f9820b296df299b56403d9ae2bd371
pwoodr01 <robert.wood@usda.gov>
2026-01-27 16:57:39 -0600

    motorcycle class

---
+++ Chapter5/SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,10 @@
+namespace SimpleClassExample;
+
+class Motorcycle
+{
+    // Method
+    public void PopAWheely()
+    {
+        System.Console.WriteLine($"The motorcycle is popping a wheely.");
+    }
+}
--- Chapter5/SimpleClassExample/Program.cs
+++ Chapter5/SimpleClassExample/Program.cs
@@ -2,11 +2,14 @@
 
 Console.WriteLine("***** Fun with Class Types *****\n");
 
+Motorcycle mc = new();
+mc.PopAWheely();
+
 // Allocate and configure a Car object.
-Car myCar = new Car();
+Car myCar = new("Zippy");
 
 // Speed up the car a few times and print out the new state.
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < 5; i++)
 {
     myCar.SpeedUp(5);
     myCar.PrintState();
