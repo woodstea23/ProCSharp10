87f2eece930172dd28727dbb0fc4e52203786c4b
pwoodr01 <robert.wood@usda.gov>
2026-02-18 15:44:00 -0600

    Chapter 8: Add CustomInterfaces project and implement CloneableType class

---
+++ Chapter8/CustomInterfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using CustomInterfaces;
+
+Console.WriteLine("A first look at Interfaces");
+CloneableExample();
+
+static void CloneableExample()
+{
+    // All of these classes support the ICloneable interface.
+    string myStr = "Hello";
+    OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+    string[] myStrArray = { "Hello", "World" };
+    int[] myIntArray = { 1, 2, 3 };
+
+    // Therefore, they can all be passed into a method taking ICloneable.
+    CloneMe(myStr);
+    CloneMe(unixOS);
+    CloneMe(myStrArray);
+    CloneMe(myIntArray);
+    static void CloneMe(ICloneable c)
+    {
+        // Clone the object and print out the type of the clone.
+        object clone = c.Clone();
+        Console.WriteLine($"Your clone is a: {clone.GetType().Name}");
+    
+        // Check if they're the same reference
+        Console.WriteLine($"Same reference? {object.ReferenceEquals(c, clone)}");
+    }
+
+}
